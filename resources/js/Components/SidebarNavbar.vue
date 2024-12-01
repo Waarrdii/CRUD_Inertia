99b1b717a327e1203ff0eaa008d9b7dbeae9624a
waarrdii <waarrdii@gmail.com>
2024-12-01 10:27:38 +0700

    setting top navbar

---
+++ resources/js/Components/SidebarNavbar.vue
@@ -0,0 +1,42 @@
+<template>
+<aside id="default-sidebar" class="fixed left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+   <div class="h-full pl-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+         <li>
+            <a @click="openNavbar('users')"
+            href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <PeopleIcon />
+               <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+            </a>
+         </li>
+         <li>
+            <a @click="openNavbar('products')"
+            href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <BagIcon />
+               <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
+            </a>
+         </li>
+      </ul>
+   </div>
+</aside>
+
+
+</template>
+
+<script setup>
+import BagIcon from '@/Icons/BagIcon.vue';
+import PeopleIcon from '@/Icons/PeopleIcon.vue';
+import { inject, provide, reactive, ref } from 'vue';
+
+const navbar = inject('navbar');
+
+const openNavbar = (newItem) => {
+    navbar.push(newItem);
+    console.log(navbar);
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Components/TopNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+        Dashboard
+    </NavLink>
+    <NavLink v-for="nav in navbar" :key="nav.id" >
+        {{ nav }}
+    </NavLink>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+import NavLink from './NavLink.vue';
+
+const navbar = inject('navbar');
+
+
+</script>
+
+<style lang="scss" scoped></style>
